c0474a84e91666c12b7f0cb3314c208829b07852
adam-belahouane <adam_belahouane@outlook.com>
2022-05-26 21:23:43 +0100

    navbar update

--- src/components/myNavbar.jsx
+++ src/components/myNavbar.jsx
@@ -1,4 +1,3 @@
-import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import "../styles/Navbar.css";
 import {
@@ -7,10 +6,11 @@ import {
   setRoleAction,
   setUserAction,
 } from "../actions";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "../FFlogo.png";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { Squash } from "hamburger-react";
 
 const MyNavbar = () => {
   const url = process.env.REACT_APP_BE_URL;
@@ -18,12 +18,19 @@ const MyNavbar = () => {
 
   const [toggleMenu, setToggleMenu] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   const isLoggedIn = useSelector((state) => state.login.isloggedin);
   const { role, user } = useSelector((state) => state.login);
 
   const dispatch = useDispatch();
+
+  window.addEventListener("resize", function(){
+    if(this.window.innerWidth > 900) {
+      setToggleMenu(false)
+      setIsOpen(false)
+    }
+  })
 
   const logout = async () => {
     if (role === "normal") {
@@ -62,6 +69,7 @@ const MyNavbar = () => {
   };
 
   return (
+    <>
     <div className="nav-bar">
       <div className="navbar-content">
         <div className="logoandhome">
@@ -72,35 +80,71 @@ const MyNavbar = () => {
         </div>
         {isLoggedIn ? (
           <div className="nav-profile" onClick={() => setShow(!show)}>
-            <img className="nav-user-img" src={user.avatar} alt="" srcset="" />
-            <span className="profile-dropdown">
-              Me
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                class="bi bi-caret-down-fill"
-                viewBox="0 0 16 16"
+            <div className="imageandme">
+              <img
+                className="nav-user-img"
+                src={user.avatar}
+                alt=""
+                srcset=""
+              />
+              <span className="profile-dropdown">
+                Me
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-caret-down-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                </svg>
+              </span>
+            </div>
+            {show ? (
+              <div
+                className="nav-dropdown-container"
+                onClick={(e) => e.stopPropagation()}
               >
-                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-              </svg>
-            </span>
-            {show?
-            
-            <div className="nav-dropdown-container" onClick={(e) => e.stopPropagation()}>
-              <div className="userdetails">
-              <img className="dropdown-profilepic" src={user.avatar} alt="user-pic" />
-              <div className="nav-nameandjob">
-              <p className="nav-name">{user.firstName} {user.lastname}</p>
-              {user.jobrole?<p className="nav-jobrole">{user.jobrole}</p>:<></>}
+                <div className="userdetails">
+                  <img
+                    className="dropdown-profilepic"
+                    src={user.avatar}
+                    alt="user-pic"
+                  />
+                  <div className="nav-nameandjob">
+                    <p className="nav-name">
+                      {user.firstName} {user.lastname}
+                    </p>
+                    {user.jobrole ? (
+                      <p className="nav-jobrole">{user.jobrole}</p>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </div>
+                {role === "pro" ? (
+                  <button
+                    className="view-profile-btn"
+                    onClick={() => navigate("/user/me")}
+                  >
+                    View Profile
+                  </button>
+                ) : (
+                  <button
+                    className="view-profile-btn"
+                    onClick={() => navigate("/user/me/norm")}
+                  >
+                    View Profile
+                  </button>
+                )}
+                <div className="signout" onClick={() => logout()}>
+                  Sign Out
+                </div>
               </div>
-              </div>
-              {role === "pro"?<button className="view-profile-btn" onClick={() => navigate("/user/me")}>View Profile</button>:<button className="view-profile-btn" onClick={() => navigate("/user/me/norm")}>View Profile</button>}
-              <div className="signout" onClick={() => logout()}>
-                Sign Out
-              </div>
-            </div>:<></>}
+            ) : (
+              <></>
+            )}
           </div>
         ) : (
           <div className="signupandlogin">
@@ -113,53 +157,24 @@ const MyNavbar = () => {
           </div>
         )}
       </div>
+      <div className="small-nav">
+      <img src={logo} className="nav-img" />
+      <Squash toggled={isOpen} toggle={() => setIsOpen(!isOpen)} hideOutline={true} onToggle={() => setToggleMenu(!toggleMenu)} duration={0.5} />
+      </div>
     </div>
+    {toggleMenu && 
+    <div className="small-nav-open">
+      <div className="small-nav-links">
+        <a href="">Home</a>
+        {isLoggedIn?<><a href="">View Profile</a>
+        <a href="">Sign Out</a>
+        </>:<><a href="">Join now</a>
+        <a href="">Sign in</a></>}
+        
+      </div>
+    </div>}
+    </>
   );
-
-  // if (isLoggedIn === false) {
-  //   return (
-  //     <Navbar bg="light" className="nav-bar" expand="lg">
-  //       <Container fluid>
-  //         <Navbar.Brand href="/"><img src={logo} className="nav-img" /></Navbar.Brand>
-  //         <Navbar.Toggle aria-controls="navbarScroll" />
-  //         <Navbar.Collapse id="navbarScroll">
-  //           <Nav
-  //             className="me-auto my-2 my-lg-0"
-  //             style={{ maxHeight: "100px" }}
-  //             navbarScroll
-  //           >
-  //             <Nav.Link href="/">Home</Nav.Link>
-  //             <NavDropdown title="Login" id="navbarScrollingDropdown">
-  //               <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-  //               <NavDropdown.Item href="/signup">Sign up</NavDropdown.Item>
-  //             </NavDropdown>
-  //           </Nav>
-  //         </Navbar.Collapse>
-  //       </Container>
-  //     </Navbar>
-  //   );
-  // } else {
-  //   return (
-  //     <Navbar bg="light" className="nav-bar" expand="lg">
-  //       <Container className="con-nav" fluid>
-  //         <Navbar.Brand href="#">Fit Find</Navbar.Brand>
-  //         <Navbar.Toggle aria-controls="navbarScroll" />
-  //         <Navbar.Collapse id="navbarScroll">
-  //           <Nav
-  //             className="me-auto my-2 my-lg-0"
-  //             style={{ maxHeight: "100px" }}
-  //             navbarScroll
-  //           >
-  //             <Nav.Link href="/">Home</Nav.Link>
-  //             {role === 'pro' ?
-  //             <Nav.Link href="/user/me">Profile</Nav.Link>:<Nav.Link href="/User/me/norm">profile</Nav.Link>}
-  //             <Nav.Link onClick={() => logout()}>Log out</Nav.Link>
-  //           </Nav>
-  //         </Navbar.Collapse>
-  //       </Container>
-  //     </Navbar>
-  //   );
-  // }
 };
 
 export default MyNavbar;
