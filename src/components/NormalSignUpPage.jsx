279798bec97f11b6ad458b7e62f0a98775a97adc
adam-belahouane <adam_belahouane@outlook.com>
2023-01-16 22:27:45 +0000

    fixes
    sign up for both pro and normal users fixed. If an email is already in use the user is told. Redirect to profile page after successful sign up implemented. Problems with rendering products and reviews lists fixed if empty

--- src/components/NormalSignUpPage.jsx
+++ src/components/NormalSignUpPage.jsx
@@ -1,8 +1,14 @@
-import { Col, Row } from "react-bootstrap";
 import { useState } from "react";
+import { getUserAction, setIsLoggedInAction, setRoleAction } from "../actions";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const NormalSignUpPage = ({ setView }) => {
   const url = process.env.REACT_APP_BE_URL;
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const id = useSelector(state => state.login.url)
   const [nextPage, setNextPage] = useState(false);
   const [user, setUser] = useState({
     firstName: "",
@@ -13,6 +19,34 @@ const NormalSignUpPage = ({ setView }) => {
     bio: "",
     role: "normal",
   });
+
+
+  const loginAction = async (details) => {
+    try {
+      const response = await fetch(`${url}/users/login`, {
+        method: "POST",
+        body: JSON.stringify(details),
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      if (response.ok) {
+        console.log("ok");
+        dispatch(getUserAction());
+        dispatch(setIsLoggedInAction(true));
+        dispatch(setRoleAction("normal"));
+        if (id === "null") {
+          navigate("/User/me/norm");
+        } else {
+          navigate(`/user/${id}`);
+        }
+      } else {
+        console.log("not ok");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Wrong credentials, try again!");
+    }
+  } 
 
   const handleSignUpInput = (field, value) => {
     setUser({
@@ -31,13 +65,23 @@ const NormalSignUpPage = ({ setView }) => {
           "Content-type": "application/json",
         },
       });
+      if (response.status == 409) {
+        alert("Sorry a User with this email already exists")
+      }
       if (response.ok) {
         console.log("All Signed up");
+        const details = {
+          email: user.email,
+          password: user.password
+        }
+        loginAction(details)
       }
     } catch (error) {
       console.log(error);
+
     }
   };
+
 
   return (
     <>
@@ -57,6 +101,51 @@ const NormalSignUpPage = ({ setView }) => {
         <div className="signup-con">
           {nextPage === false ? (
             <>
+              <form className="signup-form">
+                <h1>Sign up</h1>
+                <p>Find Fitness professionals near you</p>
+                <div className="inputbox">
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="email"
+                    required
+                    value={user.firstName}
+                    onChange={(e) =>
+                      handleSignUpInput("firstName", e.target.value)
+                    }
+                  />
+                  <label className="labelforlogin">First Name</label>
+                </div>
+                <div className="inputbox">
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="password"
+                    required
+                    value={user.lastname}
+                    onChange={(e) =>
+                      handleSignUpInput("lastname", e.target.value)
+                    }
+                  />
+                  <label className="labelforlogin">Last Name</label>
+                </div>
+
+                <button
+                  className="big-blue-btn"
+                  onClick={() => setNextPage(true)}
+                >
+                  Continue
+                </button>
+              </form>
+              <div className="login-now">
+                Already on FitFind?{" "}
+                <a href="/login" className="blue-link-highlight">
+                  Sign in
+                </a>
+              </div>
+            </>
+          ) : (
             <form className="signup-form">
               <h1>Sign up</h1>
               <p>Find Fitness professionals near you</p>
@@ -83,51 +172,6 @@ const NormalSignUpPage = ({ setView }) => {
                   }
                 />
                 <label className="labelforlogin">Password</label>
-              </div>
-
-              <button
-                className="big-blue-btn"
-                onClick={() => setNextPage(true)}
-              >
-                Continue
-              </button>
-            </form>
-            <div className="login-now">
-            Already on FitFind?{" "}
-            <a href="/login" className="blue-link-highlight">
-              Sign in
-            </a>
-          </div>
-          </>
-          ) : (
-            <form className="signup-form">
-              <h1>Sign up</h1>
-              <p>Find Fitness professionals near you</p>
-              <div className="inputbox">
-                <input
-                  type="text"
-                  name="firstname"
-                  id="email"
-                  required
-                  value={user.firstName}
-                  onChange={(e) =>
-                    handleSignUpInput("firstName", e.target.value)
-                  }
-                />
-                <label className="labelforlogin">First Name</label>
-              </div>
-              <div className="inputbox">
-                <input
-                  type="text"
-                  name="lastname"
-                  id="password"
-                  required
-                  value={user.lastname}
-                  onChange={(e) =>
-                    handleSignUpInput("lastname", e.target.value)
-                  }
-                />
-                <label className="labelforlogin">Last Name</label>
               </div>
 
               <button
